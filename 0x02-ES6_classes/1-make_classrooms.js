a0af2360ff65f45075c0351f036bff323c4acb19
Shannon <84199763+Shannon-Kioko@users.noreply.github.com>
2024-03-09 05:41:55 +0300

    Add files via upload

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,13 @@
+/**
+ * This script initializes and returns 3 ClassRoom objects.
+ */
+import ClassRoom from './0-classroom';
+
+/**
+ * Initializes and returns an array containing three ClassRoom objects.
+ *
+ * @returns {Array} Array containing three initialized ClassRoom objects.
+ */
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+}
